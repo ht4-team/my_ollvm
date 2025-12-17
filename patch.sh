fce61f5300f24054f7b1704fdb18e10d0e0b1ee3
libai2026 <1026687734@qq.com>
2025-12-17 08:48:12 +0000

    Disable LLVM_ENABLE_LLD in obfuscator patch

--- patch.sh
+++ patch.sh
@@ -8,5 +8,6 @@ git clone --depth 1 --branch 20240619 https://github.com/mstorsjo/llvm-mingw && 
   cp build-host-ollvm.sh llvm-mingw && \
   chmod +x llvm-mingw/build-host-ollvm.sh && \
   cd llvm-mingw && \
+  patch -p1 < obfuscator-CMakeLists.rev.patch && \
   patch -p1 < Dockerfile.patch && \
   patch -p1 < build-llvm.sh.patch
